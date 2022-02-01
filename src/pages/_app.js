2e7a55ebc5963be071d779e357101e894589d3b5
Mariana Stefany <marianastefanycaio@gmail.com>
2022-02-01 17:00:34 -0300

    Configuração de estilo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,32 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from './theme';
 
-export default MyApp;
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=dev-width"
+        />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
-  return <div>Clone youtube com next.js + material-UI</div>;
+  return (
+    <div>
+      Clone youtube com next.js + material-UI
+      <Button variant="container" color="primary">
+        Hello
+      </Button>
+    </div>
+  );
 }
